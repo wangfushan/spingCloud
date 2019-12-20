a06ce17b1855ab2a6a29b35cdfb31951951ae590
wangfushan <1158301022@qq.com>
2019-12-20 17:42:14 +0800

    日志配置 redis配置

--- demo/src/main/java/com/example/demo/common/redis/Redis1.java
+++ demo/src/main/java/com/example/demo/common/redis/Redis1.java
@@ -2,45 +2,99 @@ package com.example.demo.common.redis;
 
 import lombok.extern.log4j.Log4j2;
 import org.apache.commons.lang.math.RandomUtils;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
-
-/**
- * @描述: Redis缓存工具类.
- * @作者: .
- * @创建: 2014-6-13,上午11:19:24
- * @版本: V1.0
- */
 @Log4j2
-public class RedisUtils {
-
-    private static final Logger logger = LogManager.getLogger(RedisUtils.class);
+@Component
+public class Redis1 {
 
     /**
      * 默认缓存时间
      */
-    private static final int DEFAULT_CACHE_SECONDS = 60 * 60 * 1;// 单位秒 设置成一个钟
+    private   final int DEFAULT_CACHE_SECONDS = 60 * 60 * 1;// 单位秒 设置成一个钟
 
-    private static final int DEFAULT_CACHE_NULL_SECONDS = 60; //单位秒，默认临时缓存null值一分钟，防缓存穿透
+    private   final int DEFAULT_CACHE_NULL_SECONDS = 60; //单位秒，默认临时缓存null值一分钟，防缓存穿透
 
     /**
      * 随机加减时间
      */
-    private static final int RANDOM_CACHE_SECONDS = 60 * 3 * 1;
+    private   final int RANDOM_CACHE_SECONDS = 60 * 3 * 1;
+
     @Resource
-    private static RedisTemplate redisTemplate;
+    RedisTemplate<Object, Object> redisTemplate;
+
+/*
+
+    public Object get(String key) {
+        if (StringUtils.isEmpty(key)) {
+            return null;
+        }
+        return redisTemplate.opsForValue().get(key);
+    }
+
+
+    public void set(String key, String value) {
+        if (StringUtils.isEmpty(key) || StringUtils.isEmpty(value)) {
+            return;
+        }
+        redisTemplate.opsForValue().set(key, value);
+    }
+
+    */
+/**
+     * 判断一个key是否存在
+     *
+     * @param key
+     * @return
+     *//*
+
+    public  Boolean exists(String key) {
+
+        try {
+
+            return redisTemplate.hasKey(key);
+        } catch (Exception e) {
+
+        } catch (Throwable e) {
+
+        }
+        return false;
+    }
+    */
+/**
+     * 保存一个对象到redis中并指定过期时间
+     *
+     * @param key     键 . <br/>
+     * @param object  缓存对象 . <br/>
+     * @param seconds 过期时间（单位为秒）.<br/>
+     * @return true or false .
+     *//*
+
+    public  boolean set(String key, Object object, int seconds) {
+
+        try {
+            redisTemplate.opsForValue().set(key, object, seconds, TimeUnit.SECONDS);
+        } catch (Exception e) {
+
+            return false;
+        }
+        return true;
+    }
+
+*/
+
+
 
     /**
      * 删除Redis中的所有key
      */
-    public static void flushAll() {
+    public   void flushAll() {
         JedisRedisUtils.flushAll();
     }
 
@@ -52,7 +106,7 @@ public class RedisUtils {
      * @return true or false . <br/>
      * @throws Exception
      */
-    public static boolean save(Object key, Object object) {
+    public   boolean save(Object key, Object object) {
         int timeout = DEFAULT_CACHE_SECONDS;
         if (object == null) {
             // 缓存null值较短时间，既防止null穿透，又防止新增信息不能及时刷新
@@ -74,7 +128,7 @@ public class RedisUtils {
      * @param seconds 过期时间（单位为秒）.<br/>
      * @return true or false .
      */
-    public static boolean save(Object key, Object object, int seconds) {
+    public   boolean save(Object key, Object object, int seconds) {
         log.debug("保存对象到redis, key：【{}】, 【{}】 【{}】", key, object, seconds);
         try {
             redisTemplate.opsForValue().set(key, object, seconds, TimeUnit.SECONDS);
@@ -93,7 +147,7 @@ public class RedisUtils {
      * @return Object .<br/>
      * @throws Exception
      */
-    public static Object get(Object key) {
+    public   Object get(Object key) {
         return redisTemplate.opsForValue().get(key);
     }
 
@@ -104,7 +158,7 @@ public class RedisUtils {
      * @return
      * @throws Exception
      */
-    public static Boolean del(Object key) {
+    public   Boolean del(Object key) {
         log.debug("从redis删除数据, {}", key);
         try {
             redisTemplate.delete(key);
@@ -121,7 +175,7 @@ public class RedisUtils {
      * @return
      * @throws Exception
      */
-    public static Boolean del(Object... keys) {
+    public   Boolean del(Object... keys) {
         log.debug("从redis删除多个数据, {}", keys);
         try {
             redisTemplate.delete(keys);
@@ -136,7 +190,7 @@ public class RedisUtils {
      * @param seconds 超时时间（单位为秒）
      * @return
      */
-    public static Boolean expire(Object key, int seconds) {
+    public   Boolean expire(Object key, int seconds) {
         log.debug("设置redis对象超时时间, {}, {}", key, seconds);
         return redisTemplate.expire(key, seconds, TimeUnit.SECONDS);
     }
@@ -149,7 +203,7 @@ public class RedisUtils {
      * @param value
      * @return
      */
-    public static Boolean addHash(String key, Object field, Object value) {
+    public   Boolean addHash(String key, Object field, Object value) {
         log.debug("添加一个内容到指定key的hash中, {}, {}, {}", key, field, value);
         try {
             redisTemplate.opsForHash().put(key, field, value);
@@ -166,7 +220,7 @@ public class RedisUtils {
      * @param field
      * @return
      */
-    public static Object getHash(Object key, Object field) {
+    public   Object getHash(Object key, Object field) {
         log.debug("从指定hash中拿一个对象, {}, {}", key, field);
         return redisTemplate.opsForHash().get(key, field);
     }
@@ -178,7 +232,7 @@ public class RedisUtils {
      * @param field
      * @return
      */
-    public static Boolean delHash(Object key, Object field) {
+    public   Boolean delHash(Object key, Object field) {
         log.debug("从hash中删除指定filed的值, {}, {}", key, field);
         try {
             redisTemplate.opsForHash().delete(key, field);
@@ -194,7 +248,7 @@ public class RedisUtils {
      * @param pattern
      * @return
      */
-    public static Set<byte[]> keys(String pattern) {
+    public Set<Object> keys(String pattern) {
         log.debug("拿到缓存中所有符合pattern的key, {}", pattern);
         return redisTemplate.keys(pattern);
     }
@@ -205,7 +259,7 @@ public class RedisUtils {
      * @param key
      * @return
      */
-    public static Map<byte[], byte[]> getAllHash(Object key) {
+    public Map<Object, Object> getAllHash(Object key) {
         log.debug("获得hash中的所有key value, {}", key);
         return redisTemplate.opsForHash().entries(key);
     }
@@ -216,7 +270,7 @@ public class RedisUtils {
      * @param key
      * @return
      */
-    public static Boolean exists(Object key) {
+    public   Boolean exists(Object key) {
         log.debug("判断一个key是否存在:s{}", key);
         try {
             return redisTemplate.hasKey(key);
@@ -227,14 +281,8 @@ public class RedisUtils {
         }
         return false;
     }
-    @Bean
-    public static int getSize() {
+
+    public   int getSize() {
         return JedisRedisUtils.getSize();
     }
-    @Bean
-    public void setRedisTemplate(RedisTemplate redisTemplate) {
-        this.redisTemplate = redisTemplate;
-        LockUtils.setRedisTemplate(redisTemplate);
-    }
-
 }
--- demo/src/main/java/com/example/demo/controller/RemitBankColorController.java
+++ demo/src/main/java/com/example/demo/controller/RemitBankColorController.java
@@ -1,17 +1,26 @@
 package com.example.demo.controller;
 
-import com.example.demo.common.redis.JedisRedisUtils;
+
+import com.example.demo.common.redis.Redis1;
+import com.example.demo.common.util.JsonHelper;
+import com.example.demo.common.util.MapUtil;
 import com.example.demo.entity.RemitBankColor;
 import com.example.demo.service.RemitBankColorService;
+import org.apache.commons.collections.MapUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 @RestController
 @RequestMapping("/RemitBankColorController")
@@ -19,6 +28,17 @@ public class RemitBankColorController {
 
     @Autowired
     private RemitBankColorService remitBankColorService;
+    @Autowired
+    RedisTemplate<String,String> stringRedisTemplate;
+    @Autowired
+    private RedisTemplate<Object,Object> template;
+    @Autowired
+    private static RedisTemplate redisTemplate;
+
+    @Resource
+    Redis1 redis1;
+    @Autowired
+    RedisTemplate<String,String> edisTemplate;
     @PostMapping(value = "/listBankColor")
     public List<RemitBankColor> listRemit(@RequestParam("typeCode") String typeCode) {
         Map<String,Object> map=new HashMap<>();
@@ -28,15 +48,69 @@ public class RemitBankColorController {
 
     @PostMapping(value = "/getList")
     public Map<String, Object> getList(String typeCode) {
-        if (JedisRedisUtils.exists("ss")){
-            return (Map<String, Object>) JedisRedisUtils.get("ss");
-        }else{
-            Map<String, Object> map=remitBankColorService.list(typeCode);
-            JedisRedisUtils.save("ss",map,30);
+
+        if (stringRedisTemplate.hasKey("aaa11a11")) {
+            Map<Object, Object> resultMap = stringRedisTemplate.opsForHash().entries("aaa11a11");
+            String a=JsonHelper.parseToJson(resultMap);
+            return JsonHelper.parseToMap(a);
+        } else {
+            Map<String, Object> map = remitBankColorService.list(typeCode);
+            Map<String, String> mapa=new HashMap<>();
+            mapa.put("s","s");
+            stringRedisTemplate.opsForHash().putAll("aaa11a11", mapa);
             return map;
         }
+    }
 
+    @PostMapping(value = "/getList1")
+    public Map<String, Object> getList1(String typeCode) {
 
+        if (redis1.exists("11")) {
+            Map<Object, Object> resultMap = (Map<Object, Object>) redis1.get("11");
+            String a=JsonHelper.parseToJson(resultMap);
+            return JsonHelper.parseToMap(a);
+        } else {
+            Map<String, Object> map = remitBankColorService.list(typeCode);
+           // template.opsForHash().putAll("myCache",map);
+            redis1.save("11",map,111);
+
+            return map;
+        }
+    }
+
+    @PostMapping(value = "/getList2")
+    public List getList2(String typeCode) {
+
+        if (redis1.exists("11")) {
+            return (List) redis1.get("11");
+        } else {
+            Map<String, Object> map = remitBankColorService.list(typeCode);
+            // template.opsForHash().putAll("myCache",map);
+            List list=new ArrayList();
+            list.add(map.get("bankName"));
+            list.add(1);
+            list.add(1);
+            list.add(1);
+            redis1.save("11",list,111);
+            return list;
+        }
+    }
+
+    @PostMapping(value = "/getList3")
+    public String getList3(String typeCode) {
+
+        if (redis1.exists("111")) {
+            return (String) redis1.get("111");
+        } else {
+            Map<String, Object> map = remitBankColorService.list(typeCode);
+            // template.opsForHash().putAll("myCache",map);
+            if (MapUtils.getString(map,"bankName")==null){
+                map.put("bankName","sss");
+            }
+
+            redis1.save("111",map.get("bankName"),111);
+            return (String) map.get("bankName");
+        }
     }
 
 }
