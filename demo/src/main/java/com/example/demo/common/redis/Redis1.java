0efcb5a6aa745b9b3e3a11ad3234c72c4eb4f7a0
wangfushan <1158301022@qq.com>
2019-12-20 19:57:07 +0800

    redis增加lock

--- demo/src/main/java/com/example/demo/common/redis/Redis1.java
+++ demo/src/main/java/com/example/demo/common/redis/Redis1.java
@@ -285,4 +285,45 @@ public class Redis1 {
     public   int getSize() {
         return JedisRedisUtils.getSize();
     }
+
+
+    /**
+     * 获取锁
+     *
+     * @return
+     */
+    public  boolean tryLock(String lockKey, String requestId, long timeout) {
+        log.debug("请求获得锁：{}{}{}", lockKey, requestId, timeout);
+        Boolean ret = redisTemplate.opsForValue().setIfAbsent(lockKey, requestId);
+        log.debug("获得锁结果：{}", ret);
+        if (ret != null && ret) {
+            redisTemplate.expire(lockKey, timeout, TimeUnit.SECONDS);
+            log.debug("设置锁超时时间：{}{}", lockKey, timeout);
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * 解锁 需相同requestId才能解锁
+     *
+     * @param lockKey
+     * @param requestId
+     * @return
+     */
+    public  boolean unLock(String lockKey, String requestId) {
+        log.debug("请求释放锁：{}{}", lockKey, requestId);
+        Object value = redisTemplate.opsForValue().get(lockKey);
+        log.debug("查询获得锁的值：{}", value);
+        if (requestId != null && requestId.equals(value)) {
+            redisTemplate.delete(lockKey);
+            log.debug("释放锁成功：{}", lockKey);
+            return true;
+        }
+        return false;
+    }
+
+    public static void setRedisTemplate(RedisTemplate redisTemplate) {
+        redisTemplate = redisTemplate;
+    }
 }
